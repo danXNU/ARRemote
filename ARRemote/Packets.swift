bbf6bef2fc496c96e02a9fb9c55b7868034493da
DaniTox <danitox@protonmail.com>
2019-05-18 19:58:40 +0200

    Basic remote touches

---
+++ ARRemote/Packets.swift
@@ -0,0 +1,13 @@
+//
+//  Packets.swift
+//  ARRemote
+//
+//  Created by Dani Tox on 18/05/2019.
+//  Copyright © 2019 Dani Tox. All rights reserved.
+//
+
+import Foundation
+
+enum Packet: Int, Codable {
+    case touch = 1
+}
--- ARRemote/ViewController.swift
+++ ARRemote/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
 class ViewController: UIViewController {
 
+    var peerID: MCPeerID!
+    var mcSession: MCSession!
+    var mcADAssistant: MCAdvertiserAssistant!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpConnectivity()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            let mcBrowser = MCBrowserViewController(serviceType: "ARPneumatica", session: self.mcSession)
+            mcBrowser.delegate = self
+            self.present(mcBrowser, animated: true, completion: nil)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if mcSession.connectedPeers.count <= 0 { return }
+        let packet = Packet.touch
+        if let data = try? JSONEncoder().encode(packet) {
+            try? mcSession.send(data, toPeers: mcSession.connectedPeers, with: .reliable)
+        }
     }
 
-
+    func setUpConnectivity() {
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .none)
+        mcSession.delegate = self
+    }
+    
 }
 
+extension ViewController: MCSessionDelegate, MCBrowserViewControllerDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Connected")
+        case .connecting:
+            print("Connecting...")
+        case .notConnected:
+            print("Not Connected")
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {}
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {}
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {}
+    
+    
+}
